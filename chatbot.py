fca0674f72c97ed89f54276f76d25f50d65dcfe7
@jasonmesserfs <jason.wong@messerfs.com>
2021-02-19 14:08:44 +0800

    Add hello_command handler

--- chatbot.py
+++ chatbot.py
@@ -29,6 +29,7 @@ def main():
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("add", add))
     dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("hello", hello_command))
  
  
     # To start the bot:
@@ -61,6 +62,10 @@ def add(update: Update, context: CallbackContext) -> None:
     except (IndexError, ValueError):
         update.message.reply_text('Usage: /add <keyword>')
  
+def hello_command(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /hello is issued."""
+    name = context.args[0]
+    update.message.reply_text('Good day,' + name + '!')
  
  
 if __name__ == '__main__':
